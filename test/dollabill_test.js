560ffcb8199fe11eb42e19be3c584b0a0224e0e5
Constance Jiang <conzjiang@gmail.com>
2015-06-19 15:07:40 -0400

    Specs for HTMLParser constructor.

--- test/dollabill_test.js
+++ test/dollabill_test.js
@@ -24,6 +24,31 @@ describe("dollabill", function () {
   });
 });
 
+describe("HTMLParser", function () {
+  var HTMLParser = dollabill.HTMLParser;
+  var parser;
+
+  beforeEach(function () {
+    parser = {};
+  });
+
+  describe("constructor", function () {
+    it("creates an HTML element if passed angle brackets tag", function () {
+      var createEl = chai.spy.on(parser, "createEl");
+      HTMLParser.call(parser, "<div>");
+
+      expect(createEl).to.have.been.called();
+    });
+
+    it("grabs an existing HTML element if no angle brackets", function () {
+      var getEl = chai.spy.on(parser, "getEl");
+      HTMLParser.call(parser, "div");
+
+      expect(getEl).to.have.been.called();
+    });
+  });
+});
+
 describe("DOMNodeCollection", function () {
   var DOMNodeCollection = dollabill.DOMNodeCollection;
   var nodes;

7aff42d3d177c7d45ce307e88bd3963ab8a659a0
Constance Jiang <conzjiang@gmail.com>
2015-06-19 12:20:25 -0400

    dollabill.extend specs.

--- test/dollabill_test.js
+++ test/dollabill_test.js
@@ -2,8 +2,24 @@ var expect = chai.expect;
 
 describe("dollabill", function () {
   describe("::extend", function () {
-    it("extends objects", function () {
-      expect(dollabill.extend({}, { a: 1 })).to.equal({ a: 1 });
+    it("adds properties of second object to first object", function () {
+      var extended = dollabill.extend({ a: 1 }, { b: 2 });
+      expect(extended).to.eql({ a: 1, b: 2 });
+    });
+
+    it("returns the original object", function () {
+      var original = {};
+      expect(dollabill.extend(original, { a: 1 })).to.eql(original);
+    });
+
+    it("handles multiple objects", function () {
+      var multiple = dollabill.extend({ a: 1 }, { b: 2 }, { c: 3, d: 4 });
+      expect(multiple).to.eql({
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4
+      });
     });
   });
 });

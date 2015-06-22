46b86167a681c2c2f9428323e0c6144f91ee1759
Constance Jiang <conzjiang@gmail.com>
2015-06-22 16:19:47 -0400

    DNC#append specs.

--- test/dom_manip_test.js
+++ test/dom_manip_test.js
@@ -28,5 +28,68 @@
         expect(testNode1.className).to.eql("cool great");
       });
     });
+
+    describe("#append", function () {
+      it("injects the given HTML to the end of every element", function () {
+        var $el = new DOMNodeCollection([testNode1, testNode2]);
+
+        [testNode1, testNode2].forEach(function (node) {
+          node.innerHTML = "welcome";
+        });
+
+        $el.append("<h1>cool</h1>");
+
+        [testNode1, testNode2].forEach(function (node) {
+          expect(node.innerHTML).to.eql("welcome<h1>cool</h1>");
+        });
+      });
+
+      it("appends other DNC instances", function () {
+        var $el = new DOMNodeCollection([testNode1]);
+        var $toAppend = new DOMNodeCollection([testNode2]);
+
+        testNode1.innerHTML = "<h1>hi</h1>";
+        testNode2.innerHTML = "fun fun";
+
+        $el.append($toAppend);
+
+        expect(testNode1.innerHTML).to.eql("<h1>hi</h1><div>fun fun</div>");
+      });
+
+      it("appends HTML elements", function () {
+        var $el = new DOMNodeCollection([testNode1]);
+
+        testNode1.innerHTML = "<h1>hi</h1>";
+        testNode2.innerHTML = "fun fun";
+
+        $el.append(testNode2);
+
+        expect(testNode1.innerHTML).to.eql("<h1>hi</h1><div>fun fun</div>");
+      });
+
+      it("removes HTML elements to append from original spot", function () {
+        var test = document.getElementById("test-palette");
+        var originalHTML = test.innerHTML;
+
+        var $el = new DOMNodeCollection([testNode1]);
+        var puppies = document.getElementById("puppies");
+        $el.append(puppies);
+
+        expect(test.innerHTML).not.to.contain("puppies");
+        test.innerHTML = originalHTML;
+      });
+
+      it("appends HTML element's html to every element", function () {
+        var element = document.createElement("h1");
+        element.innerHTML = "hi";
+
+        var $el = new DOMNodeCollection([testNode1, testNode2]);
+        $el.append(element);
+
+        [testNode1, testNode2].forEach(function (node) {
+          expect(node.innerHTML).to.eql("<h1>hi</h1>");
+        });
+      });
+    });
   });
 })();

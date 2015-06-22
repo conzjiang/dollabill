30c460783c071eabf2d2bc1c3f43326ab5441703
Constance Jiang <conzjiang@gmail.com>
2015-06-22 15:39:34 -0400

    DNC#addClass specs.

---
+++ test/dom_manip_test.js
@@ -0,0 +1,32 @@
+(function () {
+  var expect = chai.expect;
+  var DOMNodeCollection = dollabill.DOMNodeCollection;
+
+  describe("DOM manipulation", function () {
+    var testNode1, testNode2;
+
+    beforeEach(function () {
+      testNode1 = document.createElement("div");
+      testNode2 = document.createElement("div");
+    });
+
+    describe("#addClass", function () {
+      it("adds class to every element in collection", function () {
+        var $el = new DOMNodeCollection([testNode1, testNode2]);
+        $el.addClass("fun");
+
+        [testNode1, testNode2].forEach(function (node) {
+          expect(node.className).to.eql("fun");
+        });
+      });
+
+      it("doesn't overwrite classes", function () {
+        var $el = new DOMNodeCollection([testNode1]);
+        $el.addClass("cool");
+        $el.addClass("great");
+
+        expect(testNode1.className).to.eql("cool great");
+      });
+    });
+  });
+})();
